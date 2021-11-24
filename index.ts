0f49c57d6324849b4c7b0b3eae5a4cb1454e6aa1
Ivan Bacigaluppo <ibacigaluppo@fi.uba.ar>
2021-11-23 21:50:20 -0300

    feat: created register endpoint

--- index.ts
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const routes = require('./routes')
-
-app.set('view engine', 'pug');
-app.use('/', routes())
-
-app.listen(3000);
---
+++ src/model/user.ts
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+
+interface IUser {
+  firstName: String;
+  lastName: String;
+  mail: String;
+  password: String;
+  petCategory: String;
+  petName: String;
+  petSex: String;
+}
+
+interface UserModelInterface extends mongoose.Model<UserDoc> {
+  build(attr: IUser): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+  firstName: String;
+  lastName: String;
+  mail: String;
+  password: String;
+  petCategory: String;
+  petName: String;
+  petSex: String;
+}
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  mail: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  petCategory: {
+    type: String,
+    required: true,
+    enum: ['DOG', 'CAT'],
+  },
+  petName: {
+    type: String,
+    required: true,
+  },
+  petSex: {
+    type: String,
+    required: true,
+    enum: ['MALE', 'FEMALE'],
+  },
+});
+
+userSchema.statics.build = (attr: IUser) => {
+  return new User(attr);
+};
+
+const User = mongoose.model<UserDoc, UserModelInterface>('User', userSchema);
+
+export { User };
